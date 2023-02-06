85153c9ed2ce543bfdedd7881fe942c3d553265a
Sofia <tva69144@gmail.com>
2023-02-06 13:31:32 +0200

    end

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //int[] numbers = new int[8];
+
+            //for (int a = 0; a < numbers.Length; a++)
+            //{
+            //    Console.Write($"Введите {a+1} число = ");
+            //    numbers[a] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //for (int a = 0; a < numbers.Length; a++)
+            //{
+            //    Console.WriteLine(numbers[a]);
+            //}
+
+            //Задание 2
+            //double[] numbers = new double[5] {4.6,-45.8,27.9,-6.1,2.4};
+            //for (int a = 0; a < 5; a++)
+            //{
+            //    if (numbers[a] > 0) 
+            //    {
+            //        Console.WriteLine(numbers[a]);
+            //    }
+            //}
+
+            //Задание 3
+            //int[] numbers = new int[] { -7,10,5,16,-4,18};
+            //for (int a = 5; a >= 0; a--)
+            //{
+            //    Console.WriteLine(numbers[a]);
+            //}
+
+            //Задание 4
+            //Random r1= new Random();
+            //int[] numbers = new int[12];
+            //for (int a =0 ; a <11; a++)
+            //{
+            //    numbers[a] = r1.Next(163, 190);
+            //    Console.WriteLine(numbers[a]);
+            //}
+        }
+    }
+}
