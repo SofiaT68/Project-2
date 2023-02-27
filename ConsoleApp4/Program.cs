0f54b1ad94d971d01ea5bc73ebd320e7e1c426b4
sofia <tva69144@gmail.com>
2023-02-27 21:00:03 +0200

    add new

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //int[] mass1 = new int[6];
+            //Random random = new Random();
+            //for(int i= 0;i<mass1.Length;i++)
+            //{
+            //    mass1[i] = random.Next(4,12);
+
+            //}
+            //for(int i=1;i<mass1.Length;i++)
+            //{
+            //    if (mass1[0] == mass1[i])
+            //    {
+            //        Console.WriteLine(mass1[i]);
+            //    }
+            //}
+            //Задание 2
+            //int[] mass2 = new int[15];
+            //Random r= new Random();
+            //for(int i=0; i<mass2.Length; i++)
+            //{
+            //    mass2[i] = r.Next(-100,100);
+            //}
+            //for(int i=0;i<mass2.Length; i++)
+            //{
+            //    if (mass2[i]>=0)
+            //    {
+            //        Console.WriteLine(mass2[i]);
+            //    }
+            //    if (mass2[i] > 100)
+            //    {
+
+            //        Console.WriteLine(mass2[i]);
+            //    }
+            //    if (mass2[i]%2==0)
+            //    {
+            //        Console.WriteLine(mass2[i]);
+            //    }
+
+            //}
+            //Задание3
+            //    int[] mass3 = new int[] { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //    int count = 0;
+            //    for(int i= 0; i < mass3.Length;i++)
+            //    {
+            //        if (mass3[i]<0)
+            //        {
+            //            count++;
+
+            //        } 
+            //    }
+            //    Console.WriteLine(count);
+            //Задание4
+            //int[] mass4 = new int[10];
+            //int summ = 0;
+            //Random rand = new Random();
+            //for (int i=0; i<mass4.Length; i++)
+            //{
+            //    mass4[i] = rand.Next(-100,100);
+            //}
+            //for(int i=0; i<mass4.Length; i++) 
+            //{
+            //    if (mass4[i] % 2 != 0)
+            //    {
+            //        summ += mass4[i];
+            //    }
+            //}
+            //Console.WriteLine(summ/mass4.Length);
+            //Задание5
+            int[] mass5 = new int[4];
+            Random rand = new Random();
+            for(int i=0;i<mass5.Length;i++)
+            {
+                mass5[i] = rand.Next(1,10); 
+            }
+            for(int i=0;i<mass5.Length; i++)
+            {
+                mass5[i] *= 5;
+                Console.WriteLine(mass5[i]);
+
+            }
+            
+        }
+    }
+}
