bdc8b2c5ecb661bcf288e1506b28e9bae439250f
Sophi <tva69144@gmail.com>
2023-03-06 19:48:55 +0200

    new prog

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите кол-во эллементов= ");
+            //int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+            //Random rand = new Random();
+            //int max = 0;
+            //int min = 80;
+            //int Maxind=0;
+            //int Minind=0;
+            //for (int i = 0;i<arr.Length;i++)
+            //{
+            //    arr[i] = rand.Next(1,80);
+            //    Console.WriteLine(arr[i]);
+            //}
+            // for(int i=0;i<arr.Length;i++)
+            //{
+            //    if (arr[i]<min)
+            //    {
+            //        min = arr[i];
+            //        Minind = i;
+            //    }
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+            //        Maxind = i;
+            //    }
+            //}
+            //int p= max;
+            //arr[Minind] = arr[Maxind];
+            //arr[Maxind] = p;
+            //Console.ReadKey();
+
+
+            //int[] arr = new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            //int min = 1000000000;
+            //int max = 0;
+            //for (int i = arr.Length-1; i >=0; i--)
+            //{
+            //    for (int q = i - 1; q >= 0; q--)
+            //    {
+            //        if (arr[i] > arr[q])
+            //        {
+            //            int p = arr[i];
+            //            arr[i] = arr[q];
+            //            arr[q] = p;
+            //        }
+            //    }
+            //}
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //for (int i = 0; i < arr.Length; i--)
+            //{
+            //    if (arr[i] < min)
+            //    {
+            //        min = arr[i];
+
+            //    }
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+
+            //    }
+            //}
+            //int a = max + min;
+            //Console.WriteLine($"Cумма равна: {a}");
+
+
+            //int[] arr = new int[14] {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+            //for(int i=0; i<8; i++)
+            //{ 
+            //    for(int q=i+1;q<7;q++)
+            //    {
+            //        if (arr[i] > arr[q])
+            //        {
+            //            int p = arr[i];
+            //            arr[i] = arr[q];
+            //            arr[q] = p;
+            //        }
+            //    }
+            //}
+            //for (int i = arr.Length-1; i > 6; i--)
+            //{
+            //    for (int q = i - 1; q > 6; q--) 
+            //    {
+            //        if (arr[i] > arr[q])
+            //        {
+            //            int p = arr[i];
+            //            arr[i] = arr[q];
+            //            arr[q] = p;
+            //        }
+            //    }
+            //}
+            //foreach (int i in arr) 
+            //{
+            //   Console.WriteLine(i);
+            //}
+
+
+
+
+
+        }
+    }
+}
